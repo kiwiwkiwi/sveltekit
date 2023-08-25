77ccae8134d2975cf3084138a010b52a33bb8825
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2023-08-25 21:58:18 +0000

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data;
+</script>
+
+
+ <div>
+    <ul>
+        {#each data.summaries as { slug, title }}
+            <li><a href="./{slug}">{title}</a></li>
+        {/each}
+    </ul>
+ </div>
---
+++ my-app/src/routes/data.js
@@ -0,0 +1,12 @@
+export const posts = [
+    {
+        slug: 'My tech blog',
+        title: 'The Tech that Will Change the Future',
+        content: '<p>hello</p>'
+    },
+    {
+        slug: 'my animal blog',
+        title: 'The cutest animals around',
+        content: '<p>h1</p>'
+    }
+]
